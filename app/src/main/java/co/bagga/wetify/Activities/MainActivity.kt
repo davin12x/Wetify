8c7582b2ce4d943c38887a0a3b4c2ca85154032d
Lalit Bagga <davin12x@gmail.com>
2017-06-18 21:16:32 -0400

    search activity added

--- app/src/main/java/co/bagga/wetify/Activities/MainActivity.kt
+++ app/src/main/java/co/bagga/wetify/Activities/MainActivity.kt
@@ -1,10 +1,13 @@
 package co.bagga.wetify.Activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
 import co.bagga.wetify.Adapter.WeatherAdapter
 import co.bagga.wetify.Interface.HttpCallBack
 import co.bagga.wetify.Network.RequestGenerator
@@ -12,7 +15,7 @@ import co.bagga.wetify.R
 import co.bagga.wetify.Utils.JsonParser
 import co.bagga.wetify.Utils.SharedPreference
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
     val TAG: String = javaClass.simpleName
     val weatherAdapter: WeatherAdapter = WeatherAdapter();
 
@@ -22,10 +25,22 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.elevation = 0f
 
         val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        val addCityButton = findViewById(R.id.floatingActionButton) as FloatingActionButton
+        addCityButton.setOnClickListener(this)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = weatherAdapter
 
-        fetchWeatherDataByName("Kitchener", RequestGenerator.getInstance(applicationContext))
+        val cityList = SharedPreference.getInstance(applicationContext).getSavedCityList()
+        if (cityList != null) {
+            for (citName in cityList) {
+                fetchWeatherDataByName(citName, RequestGenerator.getInstance(applicationContext))
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SearchActivity::class.java);
+        startActivity(intent);
     }
 
     fun fetchWeatherDataByName(cityName: String, requestGenerator: RequestGenerator) {
